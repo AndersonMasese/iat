691bf6924ff4ac1b8a9ebd84c7098e2a5782a8d3
andersonmasese <andersonmasese@gmail.com>
2017-04-20 00:10:53 -0700

    Zebaki app

---
+++ app/src/main/java/com/zpanel/anderson/zebaki/Success.java
@@ -0,0 +1,20 @@
+package com.zpanel.anderson.zebaki;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Success.this);
+        builder.setMessage("Operation    Successful")
+                .setNegativeButton("OK", null)
+                .create()
+                .show();
+    }
+}
